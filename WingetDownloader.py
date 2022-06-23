bf3f63eea885471ab9e7f34d2a12d16597e18c5b
Mehdi Rtal <70097433+MehdiRtal@users.noreply.github.com>
2022-06-23 11:04:00 +0100

    Update WingetDownloader.py

--- WingetDownloader.py
+++ WingetDownloader.py
@@ -12,8 +12,8 @@ parser.add_argument("-artifact", action="store_false")
 args = parser.parse_args()
 
 download_link = "https://github.com/microsoft/winget-cli/releases/latest/download/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
-root_filename = wget.filename_from_url(download_link)
-filenames_to_extract = ["winget.exe", "WindowsPackageManager.dll", "resources.pri"]
+root_file = wget.filename_from_url(download_link)
+files_to_extract = ["winget.exe", "WindowsPackageManager.dll", "resources.pri"]
 
 def version():
     response = requests.get("https://github.com/microsoft/winget-cli/releases/latest")
@@ -21,23 +21,23 @@ def version():
     return "".join(re.findall(r"\d|[.]", soup.find("h1", class_="d-inline mr-3").text))
 
 def extract(arch):
-    with zipfile.ZipFile(root_filename) as zip:
+    with zipfile.ZipFile(root_file) as zip:
         files = zip.namelist()
         for file in files:
             if f"AppInstaller_{arch}.msix" in file:
                 zip.extract(file)
-    os.remove(root_filename)
+    os.remove(root_file)
     with zipfile.ZipFile(f"AppInstaller_{arch}.msix") as zip:
-        for filename in list(set(filenames_to_extract) & set(zip.namelist())):
-            zip.extract(filename)
+        for file in list(set(files_to_extract) & set(zip.namelist())):
+            zip.extract(file)
     os.remove(f"AppInstaller_{arch}.msix")
 
 def compress(version, arch):
     with zipfile.ZipFile(f"winget_{version}_{arch}.zip", mode="w") as zip:
-        for file_to_write in filenames_to_extract:
-            zip.write(file_to_write, file_to_write, compress_type=zipfile.ZIP_STORED)
-    for filename in filenames_to_extract:
-        os.remove(filename)
+        for file in files_to_extract:
+            zip.write(file, file, compress_type=zipfile.ZIP_STORED)
+    for file in files_to_extract:
+        os.remove(file)
 
 def deploy(arch):
     extract(arch)
@@ -48,8 +48,8 @@ def deploy(arch):
             f.write(f"version={version()}")
 
 if __name__ == "__main__":
-    if os.path.exists(root_filename):
-        os.remove(root_filename)
+    if os.path.exists(root_file):
+        os.remove(root_file)
     wget.download(download_link)
     if args.arch:
         deploy(args.arch)
