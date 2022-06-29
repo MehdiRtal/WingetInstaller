c34e29bdfcab4784516533f7fc770d67ba9503c1
Mehdi Rtal <70097433+MehdiRtal@users.noreply.github.com>
2022-06-29 17:29:25 +0100

    Update WingetInstaller.py

--- WingetInstaller.py
+++ WingetInstaller.py
@@ -5,6 +5,7 @@ import os
 import argparse
 import re
 from bs4 import BeautifulSoup
+
 parser = argparse.ArgumentParser()
 parser.add_argument("architecture", nargs="?", default="x64")
 parser.add_argument("-portable", action="store_false")
@@ -13,30 +14,18 @@ parser.add_argument("-artifact", action="store_true")
 args = parser.parse_args()
 
 files = ["winget.exe", "WindowsPackageManager.dll", "resources.pri"]
-
-    
-          
-            
-    
-
-          
-          
-            
-    
-
-          
-    
-    @@ -55,9 +54,6 @@ def deploy(arch):
-  
 DLLs = ["concrt140.dll", "msvcp140.dll", "vcruntime140.dll", "vcruntime140_1.dll"]
+
 def version():
     response = requests.get("https://github.com/microsoft/winget-cli/releases/latest")
     soup = BeautifulSoup(response.text, "lxml")
     return "".join(re.findall(r"\d|[.]", soup.find("h1", class_="d-inline mr-3").text))
+
 def download():
     wget.download("https://github.com/microsoft/winget-cli/releases/latest/download/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle", bar=None)
     for DLL in DLLs:
         wget.download(f"https://github.com/MehdiRtal/WingetInstaller/raw/main/DLLs/{DLL}", bar=None)
+        
 def extract(arch):
     with zipfile.ZipFile("Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle") as zip:
         for file in zip.namelist():
@@ -47,15 +36,18 @@ def extract(arch):
             zip.extract(file)
     os.remove("Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle")
     os.remove(f"AppInstaller_{arch}.msix")
+    
 def compress(version, arch):
     with zipfile.ZipFile(f"winget_{version}_{arch}.zip", mode="w") as zip:
         for file in files + DLLs:
             zip.write(file, file, compress_type=zipfile.ZIP_STORED)
-            os.remove(file)  
+            os.remove(file)
+            
 def install():
     for file in files + DLLs:
         os.system(fr"cmd /c xcopy {file} C:\Windows\System32 /c /q /y")
         os.remove(file)
+        
 def deploy(arch):
     download()
     extract(arch)
